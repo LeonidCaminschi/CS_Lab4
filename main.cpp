f64a6ae0ec6601c305a893369c77f97c0877960f
LeonidCaminschi <leocer.leonid@gmail.com>
2023-11-14 08:17:19 +0200

    final version

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <random>
 #include <cmath>
 #include <chrono>
-#include <algorithm>
 
 using namespace std;
 
@@ -18,37 +17,36 @@ int main() {
             {46, 42, 50, 36, 29, 32}
     };
 
-    int bitsToIgnore[] = {9, 18, 22, 25, 35, 38, 43, 54};
-
     std::mt19937 generator(std::chrono::system_clock::now().time_since_epoch().count());
     std::uniform_int_distribution<uint64_t> distribution(0, pow(2,57)-1);
     uint64_t key = distribution(generator);
 
     cout << hex << key << endl;
 
-    for(int i=0; i<16; i++) {
-
         uint64_t first_28_bits = (key >> 28);
         uint64_t last_28_bits = ((key << 36) >> 36);
+
+    for(int i=0; i<16; i++) {
+        cout << dec << "Round " << i+1 << ":" << endl;
 
         cout << hex << first_28_bits << " " << last_28_bits << endl;
 
         first_28_bits = (first_28_bits << 1);
         last_28_bits = (last_28_bits << 1);
 
-        if (first_28_bits & 0x20000000) {
-            first_28_bits = first_28_bits & 0x1FFFFFFF;
+        if (first_28_bits & 0x10000000) {
+            first_28_bits = first_28_bits & 0x0FFFFFFF;
             first_28_bits = first_28_bits + 1;
         }
 
-        if (last_28_bits & 0x20000000) {
-            last_28_bits = last_28_bits & 0x1FFFFFFF;
+        if (last_28_bits & 0x10000000) {
+            last_28_bits = last_28_bits & 0x0FFFFFFF;
             last_28_bits = last_28_bits + 1;
         }
 
         uint64_t intermediary = (first_28_bits << 28) + last_28_bits;
-        cout << hex << intermediary << endl;
-        int reorderedInt = 0;
+       cout << hex << intermediary << endl;
+        uint64_t reorderedInt = 0;
 
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 6; col++) {
@@ -59,6 +57,7 @@ int main() {
         }
 
         cout << hex << reorderedInt << endl;
-        key = intermediary;
+        // key = intermediary;
+        cout << endl;
     }
 }
